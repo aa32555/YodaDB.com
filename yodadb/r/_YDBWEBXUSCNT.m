ffe1fd7ecf979313492a7bbed8bb17b870c20421
ahmed khaled <ahmedatefuzy@gmail.com>
2024-09-07 02:34:37 +0000

    ..

--- yodadb/r/_YDBWEBXUSCNT.m
+++ yodadb/r/_YDBWEBXUSCNT.m
@@ -1,7 +1,7 @@
-%YDBWEBXUSCNT ;Job counting for YodaDB; 05-07-2021 
+%YDBWEBXUSCNT ;Job counting for YottaDB; 05-07-2021 
 	;#################################################################
 	;#                                                               #
-	;# Copyright (c) 2021 YodaDB LLC and/or its subsidiaries.       #
+	;# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.       #
 	;# All rights reserved.                                          #
 	;#                                                               #
 	;#   This source code contains the intellectual property         #
