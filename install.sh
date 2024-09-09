29c1f405b89770bcf99325b63a71e5328b22b509
ahmed khaled <ahmedfota@tutanota.com>
2024-09-09 23:03:59 +0000

    i did indeed do the needful, and suddenly i have a hunger for beans

--- install.sh
+++ install.sh
@@ -11,32 +11,67 @@
          echo "must be run as root"
          echo "this installs the server database GT.M...YottaDB"
          echo "=================================================="
-         apt-get update
-         apt-get install -yq git wget gnupg
-         apt-get -y install tzdata pkg-config lsof procps
-         apt-get install -y git wget nodejs npm binutils libicu-dev libasound2
-         apt-get install -y libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
-         apt-get install -y libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 
-         apt-get install -y  build-essential cmake bison flex xxd libreadline-dev libssl-dev libconfig-dev
-         apt-get install -y libcmocka-dev
-         apt-get install -y default-jdk expect golang-go curl
-         apt-get install gawk
-         apt-get install nodejs npm
-         apt install libgcrypt20-dev
-         mkdir -p /tmp/tmp && cd /tmp/tmp
-         wget -o- https://download.yottadb.com/ydbinstall.sh && chmod +x ydbinstall.sh
-         ./ydbinstall.sh --verbose --utf8 --octo --overwrite-existing
+         echo "~~this installs the precompiled C files i.e. YottaDb from the AA32555@Github YodaDB repo."
+        #
+        #       
+        apt-get update
+        apt-get install
+        apt-get install -y git
+        apt-get install -y wget
+        apt-get install -y gnupg
+        apt-get install -y tzdata 
+        apt-get install -y pkg-config 
+        apt-get install -y lsof
+        apt-get install -y procps
+        apt-get install -y cmake 
+        apt-get install -y clang
+        apt-get install -y llvm
+        apt-get install -y lld
+        apt-get install -y make
+        apt-get install -y gcc
+        apt-get install -y git
+        apt-get install -y curl
+        apt-get install -y tcsh
+        apt-get install -y libconfig-dev
+        apt-get install -y libelf-dev
+        apt-get install -y libicu-dev
+        apt-get install -y libncurses-dev
+        apt-get install -y libreadline-dev
+        apt-get install -y binutils
+        apt-get install -y ca-certificates
+        apt-get install -y libicu-dev
+        apt-get install -y libasound2
+        apt-get install -y libnss3-dev
+        apt-get install -y libgdk-pixbuf2.0-dev
+        apt-get install -y libgtk-3-dev
+        apt-get install -y ibxss-dev
+        apt-get install -y libgconf-2-4
+        apt-get install -y libatk1.0-0
+        apt-get install -y libatk-bridge2.0-0
+        apt-get install -y libgdk-pixbuf2.0-0
+        apt-get install -y libgtk-3-0
+        apt-get install -y build-essential
+        apt-get install -y bison
+        apt-get install -y flex
+        apt-get install -y xxd
+        apt-get install -y libreadline-dev
+        apt-get install -y libssl-dev
+        apt-get install -y libconfig-dev
+        apt-get install -y libcmocka-dev
+        apt-get install -y default-jdk
+        apt-get install -y expect
+        apt-get install -y golang-go
+        apt-get install -y curl
+        apt-get install -y libgcrypt20-dev
+        apt-get install -y gawk
+        apt-get install -y nano
+        apt-get install -y htop
+        mkdir -p /tmp/tmp && cd /tmp/tmp
+        curl -o- https://raw.githubusercontent.com/aa32555/YDB/master/sr_unix/ydbinstall.sh | bash
+        chmod +x ydbinstall.sh
+        ./ydbinstall.sh --utf8 default --verbose --octo --from-source https://github.com/aa32555/YDB.git --overwrite-existing  
          echo "=================================================================================================================================="
-         echo " # replace /root/ydbcom/yodadb/r with your path to r"
-         echo "     export ydb_routines=`ydb -run %XCMD 'W $P($P($ZRO,\"(\",1,2),\")\")_\" \"_\"\/root\/ydbcom\/yodadb\/r\"_\")\"_$P($ZRO,\")\",2,$L($ZRO,\")\"))'`" 
-         echo " # needed for the web %SS"
-         echo " # put this in the begining of the routine to see the details  as well"
-         echo " # this is for future startup routinfs ...se _YDBWEB.,m for details"
-         echo "     export ydb_zinterrupt=\"I $$JOBEXAM^%YDBWEBZU($ZPOSITION)\""
-         echo " # replace 5012 with whatever port, default is 8089"
-         echo "ydb -run 'Start^%YDBWEB' 5012"
-         echo "ydb -run 'Check^%YDBWEB' >  `ydb -run %XCMD 'W +$H_".%YDBWEBLOG.txt"'`"
-         echo "=================================================================================================================================="
+         echo "YodaDB(YottaDB, let's be serious) Installer..."
          echo "Made by: Ahmed Khaled Abdelrazek"
          echo "حمد خالد محمد عبد الرازق حفيد القبطان البكري الله يمسيه بالخير"
          echo "www.YodaDb.com"
