b8c05afc8739592ed41a92bce7a86032e3667290
ahmed khaled <ahmedfota@tutanota.com>
2024-09-11 06:46:10 +0000

    ..

--- install.sh
+++ install.sh
@@ -69,5 +69,9 @@
         mkdir -p /tmp/tmp && cd /tmp/tmp
         curl -o- https://raw.githubusercontent.com/aa32555/YDB/master/sr_unix/ydbinstall.sh | bash
         chmod +x ydbinstall.sh
-        ./ydbinstall.sh --utf8 default --verbose --octo --overwrite-existing --from-source https://github.com/aa32555/YDB.git  
+        ./ydbinstall.sh --utf8 default --verbose --octo --overwrite-existing --from-source https://github.com/aa32555/YDB.git 
+        ydb -r %XCMD 'W # W "YodaDB Installer..."_$c(13,10)_"Made by: Ahmed Khaled Abdelrazek"'
+        
+
+
         
